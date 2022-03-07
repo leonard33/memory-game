4afc29a5642e492f95926d2fa8273e01969f4aaa
leonard33 <leonardkombo14@gmail.com>
2022-03-07 12:28:52 +0300

    check match function and score done

--- app.js
+++ app.js
@@ -54,13 +54,17 @@ cardArray.sort(()=> 0.5-Math.random());  //MAKE THE ARRAY RANDOM FOR GAME CHOOSI
 console.log(cardArray);
 
 const gridDisplay = document.querySelector('#grid');
+const score = document.querySelector('#score');
 
 let choosenCard=[];
+let choosenCardId =[];
+let cardsWon = [];
 
 // CREATIN THE BOARD / GRID
 function createBoard(){
     for(let i=0; i<cardArray.length; i++){
        const card= document.createElement('img');
+       card.classList.add('card')
        card.setAttribute('src','assets/blank.png')
        card.setAttribute('data-id',i)
        card.addEventListener('click',flipcard)
@@ -73,11 +77,43 @@ createBoard()
 function flipcard(){
     const cardId = this.getAttribute('data-id')  //geting the card ID from the grid  : "this." refers to card in the grid
     choosenCard.push(cardArray[cardId].name) //attaching the cardarray with card and pushing it in an array for comparison
+    console.log(choosenCard)
+    choosenCardId.push(cardId); // FINDING THE CARDS BY THEIR ID
+    console.log(choosenCardId)
     this.setAttribute('src',cardArray[cardId].img)  //assigning the images to the grid card
     if (choosenCard.length==2){
         setTimeout(checkMatch, 500)
     }
 }
 function checkMatch(){
-    console.log('you matched')
+    const gridcards = document.querySelectorAll('img') //get all the cards images in the grid
+    console.log(gridcards)
+    console.log('check for matched')
+    if(choosenCardId[0]== choosenCardId[1]){
+        gridcards[choosenCardId[0]].setAttribute('src','assets/blank.png')
+        gridcards[choosenCardId[1]].setAttribute('src','assets/blank.png')
+        alert('you have cosen the same card')
+    }
+   
+    if(choosenCard[0]== choosenCard[1]){  //check if chosen cards are alike
+        alert('you got a match')
+        gridcards[choosenCardId[0]].setAttribute('src','assets/white.png')  //use card ids to set attribute
+        gridcards[choosenCardId[1]].setAttribute('src','assets/white.png')
+        gridcards[choosenCardId[0]].removeEventListener('click',flipcard)  //Remove the event listeners after matchso as no to be clicked again
+        gridcards[choosenCardId[1]].removeEventListener('click',flipcard)
+        cardsWon.push(choosenCard);
+    }else {
+        gridcards[choosenCardId[0]].setAttribute('src','assets/blank.png')  //flip them back if not a match
+        gridcards[choosenCardId[1]].setAttribute('src','assets/blank.png')
+        alert('sorry try again')
+    }
+     score.textContent = cardsWon.length
+    // START THE WHOLE PROCESS AGAIN
+     choosenCard=[];
+     choosenCardId =[];
+
+     if(cardsWon.length== cardArray.length/2){
+         score.innerHTML = 'Congratulations you Won'
+     }
 }
+
